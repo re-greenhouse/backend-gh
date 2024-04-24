0263848805b721f6d14b47a17748625dfabae27e
Andrés Leiva <aleiva1700@gmail.com>
2024-04-24 16:46:45 -0500

    feat(api-gateway): add global pipe

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GrpcToHttpInterceptor } from 'nestjs-grpc-exceptions';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new GrpcToHttpInterceptor());
-  // app.useGlobalGuards(new AccessTokenGuard());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
