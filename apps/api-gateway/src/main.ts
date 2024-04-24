824a04d022e619c9da4465df5bdfe30159812488
Andrés Leiva <aleiva1700@gmail.com>
2024-04-24 10:25:48 -0500

    feat(cors): add cors configuration in api-gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { GrpcToHttpInterceptor } from 'nestjs-grpc-exceptions';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new GrpcToHttpInterceptor());
   // app.useGlobalGuards(new AccessTokenGuard());
   await app.listen(3000);
