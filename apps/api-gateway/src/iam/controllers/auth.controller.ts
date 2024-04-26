b8f717c1b078e3fede6e150e448aaa0af383565f
Andrés Leiva <aleiva1700@gmail.com>
2024-04-26 15:26:23 -0500

    feat(open-api): add first implementation of swagger doc

--- apps/api-gateway/src/iam/controllers/auth.controller.ts
+++ apps/api-gateway/src/iam/controllers/auth.controller.ts
@@ -1,15 +1,26 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
-import { SignInDto, SignUpDto } from '@app/common';
+import { AuthenticatedUser, SignInDto, SignUpDto } from '@app/common';
 import { Public } from '../../shared/decorators/public.decorator';
+import {
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
 
-@Controller('auth')
+@ApiTags('Auth')
+@Controller('api/v1/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
   @Post('sign-in')
   @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User signed token',
+  })
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
