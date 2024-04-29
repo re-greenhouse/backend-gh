87e3da0fe32366d0285f5d227b50e23c479ee15d
Andrés Leiva <aleiva1700@gmail.com>
2024-04-28 23:10:03 -0500

    feat(api-gateway): update auth controller typing

--- apps/api-gateway/src/iam/controllers/auth.controller.ts
+++ apps/api-gateway/src/iam/controllers/auth.controller.ts
@@ -1,18 +1,20 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
-import { AuthenticatedUser, SignInDto, SignUpDto } from '@app/common';
+import { SignInDto } from '@app/common';
 import { Public } from '../../shared/decorators/public.decorator';
-import {
-  ApiOkResponse,
-  ApiResponse,
-  ApiTags,
-  getSchemaPath,
-} from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { SignUpRequestDto } from '../dtos/SignUpRequest.dto';
+import { ProfileFacadeService } from '../../personas/facades/profile-facade.service';
+import { firstValueFrom } from 'rxjs';
+import { SignUpResponseDto } from '../dtos/SignUpResponse.dto';
 
 @ApiTags('Auth')
 @Controller('api/v1/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly profileFacadeService: ProfileFacadeService,
+  ) {}
 
   @Public()
   @Post('sign-in')
@@ -27,7 +29,24 @@ export class AuthController {
 
   @Public()
   @Post('sign-up')
-  signUp(@Body() signUpDto: SignUpDto) {
-    return this.authService.signUp(signUpDto);
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Created user and profile',
+    type: SignUpResponseDto,
+  })
+  async signUp(
+    @Body() signUpDto: SignUpRequestDto,
+  ): Promise<SignUpResponseDto> {
+    const user = await firstValueFrom(this.authService.signUp(signUpDto));
+    const profile = await firstValueFrom(
+      this.profileFacadeService.create({
+        userId: user.id,
+        firstName: signUpDto.firstName,
+        lastName: signUpDto.lastName,
+        iconUrl:
+          'https://publicdomainvectors.org/tn_img/abstract-user-flat-4.webp',
+      }),
+    );
+    return { user: user, profile: profile };
   }
 }
