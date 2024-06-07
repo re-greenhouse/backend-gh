4612033580627e7d5997c93b242d9f3877f69825
Andrés Leiva <aleiva1700@gmail.com>
2024-06-07 18:20:17 -0500

    feat(repositories): add find profiles by company id

--- apps/personas/src/profiles/application/ports/find-profiles.repository.ts
+++ apps/personas/src/profiles/application/ports/find-profiles.repository.ts
@@ -4,5 +4,6 @@ export abstract class FindProfilesRepository {
   abstract findAll(): Promise<Array<Profile>>;
   abstract findById(id: string): Promise<Profile | undefined>;
   abstract findByUserId(userId: string): Promise<Profile | undefined>;
+  abstract findByCompanyId(companyId: string): Promise<Array<Profile>>;
   abstract existByUserId(userId: string): Promise<boolean>;
 }
