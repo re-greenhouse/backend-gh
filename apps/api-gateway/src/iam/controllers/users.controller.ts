045a6e543755e82a96d32725fc5c10e2a67f97c8
Andrés Leiva <aleiva1700@gmail.com>
2024-10-23 22:02:22 -0500

    feat(docs): add response type

--- apps/api-gateway/src/iam/controllers/users.controller.ts
+++ apps/api-gateway/src/iam/controllers/users.controller.ts
@@ -13,7 +13,8 @@ import { UsersService } from '../services/users.service';
 import { CreateUserDto, UpdateUserDto } from '@app/common';
 import { Roles } from '../../shared/decorators/role.decorator';
 import { Role } from '../../shared/enums/roles.enum';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UserDto } from '../dtos/User.dto';
 
 @ApiBearerAuth()
 @ApiTags('Users')
@@ -23,6 +24,11 @@ export class UsersController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'User created',
+    type: UserDto,
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -30,18 +36,34 @@ export class UsersController {
   @Get()
   @Roles(Role.Admin) // Custom decorator
   @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Retrieve all users',
+    type: UserDto,
+    isArray: true,
+  })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Roles(Role.Admin)
   @Get(':username')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Retrieve user by username',
+    type: UserDto,
+  })
   findOne(@Param('username') username: string) {
     return this.usersService.findOne(username);
   }
   // If request is from same user, OK
   // If request is from admin, OK
   @Patch(':username')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Update a user by username',
+    type: UserDto,
+  })
   update(
     @Param('username') username: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -51,6 +73,11 @@ export class UsersController {
   // If request is from same user, OK
   // If request is from admin, OK
   @Delete(':username')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Delete a user by username',
+    type: UserDto,
+  })
   remove(@Param('username') username: string) {
     return this.usersService.remove(username);
   }
